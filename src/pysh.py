444edbc7407c7aa4fce20f4cd6ca045708cc5069
Greg Huber <gregoryh@uvic.ca>
2021-06-04 17:33:23 -0700

    Piping sorta kinda works. Some things break

--- src/pysh.py
+++ src/pysh.py
@@ -3,6 +3,7 @@ from importlib import machinery
 import os
 import types
 import subprocess
+import shlex
 # Constants to use
 HOME = os.path.expanduser("~")
 # TODO this should be in a better place
@@ -50,16 +51,14 @@ def resolve(name):
     for path in PATH:
         if os.path.isfile(exec_path := os.path.join(path, name)):
             def func(arg_list=[], std_in=None, std_out=None, std_err=None):
-                process = subprocess.run([exec_path, *arg_list],
-                                         stdin=std_in,
-                                         stdout=std_out,
-                                         stderr=std_err,
-                                         universal_newlines=True,
-                                         bufsize=0)
+                process = subprocess.Popen([exec_path, *arg_list],
+                                           stdin=std_in if std_in is not None else subprocess.PIPE,
+                                           stdout=subprocess.PIPE if std_out is not None else None,
+                                           stderr=subprocess.PIPE if std_err is not None else None,
+                                           universal_newlines=True,
+                                           bufsize=80)
 
                 return process
-
-                # return process.stdout.readlines(), process.stderr.readlines()
             return func
     # If not in path try to call the function from config
     if config_func := getattr(config, name, None):
@@ -74,6 +73,19 @@ def render_prompt() -> str:
     return PROMPT if ERROR == 0 else f'(%d) %s' % (ERROR, PROMPT)
 
 
+def pipe_index_generator(arr: list[str]) -> tuple[int, int]:
+    start = 0
+    end = 0
+
+    for i in range(len(arr)):
+        if arr[i] == '|':
+            end = i
+            yield start, end
+            start = i + 1
+
+    yield start, len(arr)
+
+
 if __name__ == "__main__":
     init()
 
@@ -83,11 +95,26 @@ if __name__ == "__main__":
         if command.strip().lower() == "exit":
             exit(0)
         else:
-            [exec_name, *args] = command.split(' ')
-            exec_function = resolve(exec_name)
+            # TODO for now assume " | " with spaces is pipe, eventually fix this
+            arg_list = shlex.split(command)
+            pipe_split_commands = [arg_list[s:e]
+                                   for s, e in pipe_index_generator(arg_list)]
 
-            if exec_function is not None:
-                exec_function()
-            else:
-                print("command not found")
-                ERROR = 404
+            last_process = None
+
+            for num, [exec_name, *args] in enumerate(pipe_split_commands):
+                exec_function = resolve(exec_name)
+                # TODO this currently breaks for interactive processes
+                if exec_function is not None:
+                    last_process = exec_function(arg_list=args,
+                                                 std_in=None if last_process is None else last_process.stdout,
+                                                 std_out=True if num < len(
+                                                     pipe_split_commands) - 1 else None,
+                                                 std_err=None)
+                else:
+                    print("command not found")
+                    ERROR = 404
+
+            if last_process is not None:
+                last_process.communicate()
+                ERROR = last_process.returncode
